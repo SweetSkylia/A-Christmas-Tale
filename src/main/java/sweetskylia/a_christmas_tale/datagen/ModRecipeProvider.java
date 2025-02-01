fc9c44eb02de68432ddcc40fd0b92dae7279e003
SweetSkylia <146096843+SweetSkylia@users.noreply.github.com>
2025-02-01 22:42:28 +0100

    Added more Recipe

--- src/main/java/sweetskylia/a_christmas_tale/datagen/ModRecipeProvider.java
+++ src/main/java/sweetskylia/a_christmas_tale/datagen/ModRecipeProvider.java
@@ -5,11 +5,13 @@ import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
 import net.minecraft.data.server.recipe.RecipeExporter;
 import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
 import net.minecraft.data.server.recipe.SmithingTransformRecipeJsonBuilder;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemConvertible;
 import net.minecraft.item.Items;
 import net.minecraft.recipe.Ingredient;
 import net.minecraft.recipe.book.RecipeCategory;
 import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.registry.tag.ItemTags;
 import net.minecraft.util.Identifier;
 import sweetskylia.a_christmas_tale.AChristmasTale;
 import sweetskylia.a_christmas_tale.block.ModBlocks;
@@ -26,24 +28,68 @@ public class ModRecipeProvider extends FabricRecipeProvider {
     @Override
     public void generate(RecipeExporter exporter) {
 
-        List<ItemConvertible> STELLAR_HEART_SMELTABLES = List.of(ModBlocks.STELLAR_HEART_ORE,ModItems.FROZEN_STELLAR_HEART);
+        List<ItemConvertible> STELLAR_HEART_SMELTABLES = List.of(ModBlocks.STELLAR_HEART_ORE, ModItems.FROZEN_STELLAR_HEART);
 
-       offerSmelting(exporter, STELLAR_HEART_SMELTABLES, RecipeCategory.MISC,ModItems.STELLAR_HEART,0.25f,200, "stellar_heart");
-       offerBlasting(exporter, STELLAR_HEART_SMELTABLES, RecipeCategory.MISC,ModItems.STELLAR_HEART,0.25f,100, "stellar_heart");
+        //Stellar Heart Ore Smelting
+        offerSmelting(exporter, STELLAR_HEART_SMELTABLES, RecipeCategory.MISC, ModItems.STELLAR_HEART, 0.25f, 200, "stellar_heart");
+        offerBlasting(exporter, STELLAR_HEART_SMELTABLES, RecipeCategory.MISC, ModItems.STELLAR_HEART, 0.25f, 100, "stellar_heart");
 
-       offerReversibleCompactingRecipes(exporter, RecipeCategory.FOOD, ModItems.CANDY, RecipeCategory.BUILDING_BLOCKS, ModBlocks.CANDY_BLOCK);
+        //Stellar Block
+        offerReversibleCompactingRecipes(exporter,RecipeCategory.MISC, ModItems.STELLAR_HEART,RecipeCategory.BUILDING_BLOCKS, ModBlocks.STELLAR_BLOCK);
+        //Frozen Stellar Block
+        offerReversibleCompactingRecipes(exporter,RecipeCategory.MISC, ModItems.FROZEN_STELLAR_HEART,RecipeCategory.BUILDING_BLOCKS, ModBlocks.FROZEN_STELLAR_BLOCK);
 
-        SmithingTransformRecipeJsonBuilder.create(Ingredient.ofItems(Items.PAPER),Ingredient.ofItems(Items.IRON_SWORD),Ingredient.ofItems(ModItems.FROZEN_STELLAR_HEART), RecipeCategory.TOOLS, ModItems.FROZEN_SWORD)
+        //Winter Carpet
+        ShapedRecipeJsonBuilder.create(RecipeCategory.DECORATIONS, ModBlocks.WINTER_CARPET, 3)
+                .pattern("WW")
+                .input('W', ModBlocks.WINTER_WOOL)
+                .criterion(hasItem(ModBlocks.WINTER_WOOL), conditionsFromItem(ModBlocks.WINTER_WOOL))
+                .offerTo(exporter);
+        //Christmas Carpet
+        ShapedRecipeJsonBuilder.create(RecipeCategory.DECORATIONS, ModBlocks.CHRISTMAS_CARPET, 3)
+                .pattern("WW")
+                .input('W', ModBlocks.CHRISTMAS_WOOL)
+                .criterion(hasItem(ModBlocks.CHRISTMAS_WOOL), conditionsFromItem(ModBlocks.CHRISTMAS_WOOL))
+                .offerTo(exporter);
+
+        //Christmas Wool
+        ShapedRecipeJsonBuilder.create(RecipeCategory.BUILDING_BLOCKS, ModBlocks.CHRISTMAS_WOOL, 8)
+                .pattern("WWW")
+                .pattern("WHW")
+                .pattern("WWW")
+                .input('W', Ingredient.fromTag(ItemTags.WOOL))
+                .input('H', ModItems.STELLAR_HEART)
+                .criterion(hasItem(ModItems.STELLAR_HEART), conditionsFromItem(ModItems.STELLAR_HEART))
+                .offerTo(exporter);
+
+        //Wintel Wool
+        ShapedRecipeJsonBuilder.create(RecipeCategory.BUILDING_BLOCKS, ModBlocks.WINTER_WOOL, 8)
+                .pattern("WWW")
+                .pattern("WHW")
+                .pattern("WWW")
+                .input('W', Ingredient.fromTag(ItemTags.WOOL))
+                .input('H', ModItems.FROZEN_STELLAR_HEART)
+                .criterion(hasItem(ModItems.FROZEN_STELLAR_HEART), conditionsFromItem(ModItems.FROZEN_STELLAR_HEART))
+                .offerTo(exporter);
+
+
+        //Candy / Smelting
+        offerSmelting(exporter,List.of(Items.SUGAR), RecipeCategory.FOOD, ModItems.CANDY, 0.1f, 200, "candy_from_smelting");
+        //Candy / CandyBlock
+        offerReversibleCompactingRecipes(exporter, RecipeCategory.FOOD, ModItems.CANDY, RecipeCategory.BUILDING_BLOCKS, ModBlocks.CANDY_BLOCK);
+
+        //Frozen Sword
+        SmithingTransformRecipeJsonBuilder.create(Ingredient.ofItems(Items.PAPER), Ingredient.ofItems(Items.IRON_SWORD), Ingredient.ofItems(ModItems.FROZEN_STELLAR_HEART), RecipeCategory.TOOLS, ModItems.FROZEN_SWORD)
                 .criterion(hasItem(ModItems.FROZEN_STELLAR_HEART), conditionsFromItem(ModItems.FROZEN_STELLAR_HEART))
                 .offerTo(exporter, Identifier.of(AChristmasTale.MOD_ID, "frozen_sword_from_smithing"));
 
 
-
-        ShapedRecipeJsonBuilder.create(RecipeCategory.FOOD,ModItems.HOT_CHOCOLATE)
+        //Hot Chocolate
+        ShapedRecipeJsonBuilder.create(RecipeCategory.FOOD, ModItems.HOT_CHOCOLATE)
                 .pattern("SC")
                 .input('S', Items.SUGAR)
                 .input('C', Items.COCOA_BEANS)
-                .criterion(hasItem(Items.COCOA_BEANS),conditionsFromItem(Items.COCOA_BEANS))
+                .criterion(hasItem(Items.COCOA_BEANS), conditionsFromItem(Items.COCOA_BEANS))
                 .offerTo(exporter);
     }
 }
