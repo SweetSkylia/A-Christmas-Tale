ae22ed01f552d194ae97af272c537ac41d09f0e1
SweetSkylia <146096843+SweetSkylia@users.noreply.github.com>
2025-02-25 16:06:15 +0100

    Trying to improve present block
    I'm failing to create a specific blockstate that will change the texture of the present

--- src/main/java/sweetskylia/a_christmas_tale/block/custom/PresentBlock.java
+++ src/main/java/sweetskylia/a_christmas_tale/block/custom/PresentBlock.java
@@ -6,17 +6,23 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.HorizontalFacingBlock;
 import net.minecraft.block.ShapeContext;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.state.StateManager;
+import net.minecraft.state.property.IntProperty;
+import net.minecraft.state.property.Property;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
 public class PresentBlock extends HorizontalFacingBlock {
     public static final MapCodec<PresentBlock> CODEC = createCodec(PresentBlock::new);
     private static final VoxelShape SHAPE = Block.createCuboidShape(0,0,0,16,8,16);
-
+    public static final IntProperty STATE = IntProperty.of("state", 0, 4);
 
     public PresentBlock(Settings settings) {
         super(settings);
@@ -29,16 +35,31 @@ public class PresentBlock extends HorizontalFacingBlock {
 
     @Override
     protected MapCodec<? extends HorizontalFacingBlock> getCodec() {
-        return null;
+        return CODEC;
     }
 
     @Override
     public @Nullable BlockState getPlacementState(ItemPlacementContext ctx) {
-        return this.getDefaultState().with(FACING, ctx.getHorizontalPlayerFacing().getOpposite());
+        return this.getDefaultState()
+                .with(FACING, ctx.getHorizontalPlayerFacing().getOpposite())
+                .with(STATE, 0);
     }
 
     @Override
     protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-        builder.add(FACING);
+        builder.add(FACING, STATE);
+    }
+
+    @Override
+    protected ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, BlockHitResult hit) {
+        if (!world.isClient) {
+            int currentState = state.get(STATE);
+            if (currentState < 4){
+                int newState = currentState + 1;
+                world.setBlockState(pos, state.with(STATE, newState), Block.NOTIFY_ALL);
+            }
+        }
+
+        return super.onUse(state, world, pos, player, hit);
     }
 }
