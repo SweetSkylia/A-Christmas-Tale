75f5566fa5e7bc76c72e356077cb6247d32d5c3e
SweetSkylia <146096843+SweetSkylia@users.noreply.github.com>
2025-02-28 15:53:31 +0100

    Waterlog present

--- src/main/java/sweetskylia/a_christmas_tale/block/custom/PresentBlock.java
+++ src/main/java/sweetskylia/a_christmas_tale/block/custom/PresentBlock.java
@@ -1,28 +1,41 @@
 package sweetskylia.a_christmas_tale.block.custom;
 
 import com.mojang.serialization.MapCodec;
-import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.HorizontalFacingBlock;
 import net.minecraft.block.ShapeContext;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.state.property.IntProperty;
-import net.minecraft.state.property.Property;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.ItemActionResult;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldAccess;
 import org.jetbrains.annotations.Nullable;
 
 public class PresentBlock extends HorizontalFacingBlock {
     public static final MapCodec<PresentBlock> CODEC = createCodec(PresentBlock::new);
     private static final VoxelShape SHAPE = Block.createCuboidShape(0,0,0,16,8,16);
     public static final IntProperty STATE = IntProperty.of("state", 0, 4);
+    public static final BooleanProperty WATERLOGGED = Properties.WATERLOGGED;
 
     public PresentBlock(Settings settings) {
         super(settings);
@@ -40,25 +53,50 @@ public class PresentBlock extends HorizontalFacingBlock {
 
     @Override
     public @Nullable BlockState getPlacementState(ItemPlacementContext ctx) {
+        World world = ctx.getWorld();
+        BlockPos pos = ctx.getBlockPos();
+        boolean isWater = world.getFluidState(pos).getFluid() == Fluids.WATER;
         return this.getDefaultState()
                 .with(FACING, ctx.getHorizontalPlayerFacing().getOpposite())
-                .with(STATE, 0);
+                .with(STATE, 0)
+                .with(WATERLOGGED, isWater);
     }
 
     @Override
     protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-        builder.add(FACING, STATE);
+        builder.add(FACING, STATE, WATERLOGGED);
+    }
+
+    @Override
+    protected FluidState getFluidState(BlockState state) {
+        return state.get(WATERLOGGED) ? Fluids.WATER.getStill(false) : super.getFluidState(state);
+    }
+
+    @Override
+    public void onStateReplaced(BlockState state, World world, BlockPos pos, BlockState newState, boolean moved) {
+        if (state.get(WATERLOGGED)) {
+            world.scheduleFluidTick(pos, Fluids.WATER, Fluids.WATER.getTickRate(world));
+        }
+    }
+
+
+    @Override
+    protected BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos) {
+        if (state.get(WATERLOGGED)) {
+            world.scheduleFluidTick(pos, Fluids.WATER, Fluids.WATER.getTickRate(world));
+        }
+        return super.getStateForNeighborUpdate(state, direction, neighborState, world, pos, neighborPos);
     }
 
     @Override
     protected ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, BlockHitResult hit) {
-        if (!world.isClient) {
-            int currentState = state.get(STATE);
-            if (currentState < 4){
-                int newState = currentState + 1;
-                world.setBlockState(pos, state.with(STATE, newState), Block.NOTIFY_ALL);
-            }
-        }
+//        if (!world.isClient) {
+//            int currentState = state.get(STATE);
+//            if (currentState < 4){
+//                int newState = currentState + 1;
+//                world.setBlockState(pos, state.with(STATE, newState), Block.NOTIFY_ALL);
+//            }
+//        }
 
         return super.onUse(state, world, pos, player, hit);
     }
