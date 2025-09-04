b70990eeb2a9417baa8ae8a01910c8dd3eab7e84
SweetSkylia <146096843+SweetSkylia@users.noreply.github.com>
2025-09-04 22:56:07 +0200

    1.21 -> 1.21.3 Datagen & WorldGen

--- src/main/java/sweetskylia/a_christmas_tale/datagen/ModRegistryDataGenerator.java
+++ src/main/java/sweetskylia/a_christmas_tale/datagen/ModRegistryDataGenerator.java
@@ -14,12 +14,12 @@ public class ModRegistryDataGenerator extends FabricDynamicRegistryProvider {
 
     @Override
     protected void configure(RegistryWrapper.WrapperLookup registries, Entries entries) {
-            entries.addAll(registries.getWrapperOrThrow(RegistryKeys.CONFIGURED_FEATURE));
-            entries.addAll(registries.getWrapperOrThrow(RegistryKeys.PLACED_FEATURE));
+            entries.addAll(registries.getOrThrow(RegistryKeys.CONFIGURED_FEATURE));
+            entries.addAll(registries.getOrThrow(RegistryKeys.PLACED_FEATURE));
     }
 
     @Override
     public String getName() {
-        return "";
+        return "Registry Data Generator";
     }
 }
